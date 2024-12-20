c6ad96a0438a96f33e6f5cb087767e0c22f63801
Siddhant Shinde <siddhantshinde75576@gmail.com>
2024-12-20 19:40:25 +0530

    update

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-const length = document.getElementById("length").value;
-const includelowercase = document.getElementById("lowercase");
-const includeuppercase = document.getElementById("uppercase");
-const includenumber = document.getElementById("number");
-const includesymbols = document.getElementById("symbols");
-const displaythis = document.getElementById("displaypassword");
-function generatepassword(  length,includelowercase,includeuppercase, includedigits, includesymbols){
-    
+let symbollist = "!@#$%"
+let uclist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let lclist = "abcdefghijklmnopqrstuvwxyz"
+let numlist = "0123456789"
+let ans = "";
 
-    if(length===0){
-        displaythis.textContent = `hi`;
+$("#submit").click(function(){
+    ans=""
+    let length = $("#length").val();
+    let UCcheck = $("#uppercase").prop("checked");
+    let LCcheck = $("#lowercase").prop("checked");
+    let numcheck = $("#number").prop("checked");
+    let symbolcheck = $("#symbols").prop("checked");
+    while(length!=0){
+        let a = Math.floor(Math.random()*4);
+        if(a==0 && UCcheck){
+            adduc();
+            length--;
+        }
+        else if(a==1 && LCcheck){
+            addlc();
+            length--;
+        }
+        else if(a==2 && numcheck){
+            addnum();
+            length--;
+        }
+        else if(a==3 && symbolcheck){
+            addsymbol();
+            length--;
+        }
     }
+    $("#displaypassword").text("Your Password : "+ans);
+})
+
+function adduc(){
+    let b = Math.floor(Math.random()*26);
+    ans+=uclist[b];
 }
 
+function addlc(){
+    let b = Math.floor(Math.random()*26);
+    ans+=lclist[b];
+}
 
-function generatepassword(){
-    generatepassword(length,includelowercase,includeuppercase, includedigits, includesymbols);
+function addnum(){
+    let b = Math.floor(Math.random()*10);
+    ans+=numlist[b];
+}
+
+function addsymbol(){
+    let b = Math.floor(Math.random()*5);
+    ans+=symbollist[b];
 }
